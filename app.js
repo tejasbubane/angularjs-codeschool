33e29553f39ea110fbc1c1db60900b9730a69d4b
Tejas Bubane <tejas.bubane@infibeam.net>
2014-11-14 12:33:09 +0530

    Complete level 2.

--- app.js
+++ app.js
@@ -5,11 +5,32 @@
 	this.products = gems;
     });
 
+    app.controller("PanelController", function(){
+	this.tab = 1;
+
+	this.selectTab = function(setTab) {
+	    this.tab = setTab;
+	};
+	this.isSelected = function(checkTab) {
+	    return this.tab === checkTab;
+	};
+    });
+
     var gems = [
 	{
 	    name: 'Ruby',
 	    price: 2.95,
 	    description: 'I love Ruby!',
+	    images: [
+		{
+		    full: 'ruby-01-full.jpg',
+		    thumb: 'ruby-01-thumb.jpg'
+		},
+		{
+		    full: 'ruby-02-full.jpg',
+		    thumb: 'ruby-02-thumb.jpg'
+		}
+	    ],
 	    canPurchase: true,
 	    soldOut: false
 	},
