1ade5b55c158ad65deea00e1c2f77feffd206900
Tejas Bubane <tejas.bubane@infibeam.net>
2014-11-14 15:02:40 +0530

    Complete level 3.

--- app.js
+++ app.js
@@ -16,6 +16,15 @@
 	};
     });
 
+    app.controller("ReviewController", function() {
+	this.review = {};
+
+	this.addReview = function(product) {
+	    product.reviews.push(this.review);
+	    this.review = {};
+	};
+    });
+
     var gems = [
 	{
 	    name: 'Ruby',
@@ -29,6 +38,18 @@
 		{
 		    full: 'ruby-02-full.jpg',
 		    thumb: 'ruby-02-thumb.jpg'
+		}
+	    ],
+	    reviews: [
+		{
+		    stars: 5,
+		    body: "I love this product",
+		    author: "joe@thomas.com"
+		},
+		{
+		    stars: 1,
+		    body: "This product sucks",
+		    author: "tim@hater.com"
 		}
 	    ],
 	    canPurchase: true,
