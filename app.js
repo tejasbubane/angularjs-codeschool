a509e02538561ab4c556069514c64d3176030142
Tejas Bubane <tejas.bubane@infibeam.net>
2014-11-14 18:12:01 +0530

    Complete level 4.

--- app.js
+++ app.js
@@ -5,23 +5,37 @@
 	this.products = gems;
     });
 
-    app.controller("PanelController", function(){
-	this.tab = 1;
-
-	this.selectTab = function(setTab) {
-	    this.tab = setTab;
-	};
-	this.isSelected = function(checkTab) {
-	    return this.tab === checkTab;
-	};
-    });
-
     app.controller("ReviewController", function() {
 	this.review = {};
 
 	this.addReview = function(product) {
 	    product.reviews.push(this.review);
 	    this.review = {};
+	};
+    });
+
+    app.directive('productTitle', function(){
+	return {
+	    restrict: 'E', // E stands for element (new html element)
+	    templateUrl: 'product-title.html'
+	};
+    });
+
+    app.directive('productPanels', function() {
+	return {
+	    restrict: 'E',
+	    templateUrl: 'product-panels.html',
+	    controller: function(){
+		this.tab = 1;
+
+		this.selectTab = function(setTab) {
+		    this.tab = setTab;
+		};
+		this.isSelected = function(checkTab) {
+		    return this.tab === checkTab;
+		};
+	    },
+	    controllerAs: "panels"
 	};
     });
 
